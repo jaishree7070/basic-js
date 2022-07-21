edbcdbdaa2ea1dd306c77ded30a5bbc2873baba5
jaishree7070 <109673030+jaishree7070@users.noreply.github.com>
2022-07-21 12:01:20 +0530

    replaced console
    now the output will be displayed in the screen instead of the console pannel

--- basicjs/eight.js
+++ basicjs/eight.js
@@ -6,11 +6,12 @@ function pyramid()
     {
         for(var j=1;j<=i;j++)
         {
-            output=output+"*";//5,4,3,2,1
+            output=output+"*";
         }
-        console.log(output);
-        output='';
+        output+="<br>";
+     
     }
+    document.getElementById("result").innerHTML = "The desired output is"+"<br>" + output;
 }
 //5
 // *****
